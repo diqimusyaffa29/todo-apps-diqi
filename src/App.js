86d5c1b3a33648a57e71766624390fe592d9f97b
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2023-09-27 18:13:31 +0800

    Membuat sebuah aplikasi Todo Apps dan optimalisasi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import { TextField } from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import "./App.css";
+
+// Data Yang ditampilkan
+// const Data = [
+//   {
+//     message: 'Cari Makan',
+//     id: 1
+//   },
+//   {
+//     message: 'Belajar React',
+//     id: 2
+//   },
+//   {
+//     message: 'Belajar Lainnya',
+//     id: 3
+//   },
+// ]
+
+// Header
+const Header = () => {
+  return <>
+    <h1> <span role="img" aria-label="note">Aplikasi Taskku📝</span></h1>
+  </>
+}
+
+
+// Info Bar
+const InfoBar = ({ taskNumber }) => {
+  return <div>Ada {taskNumber} yang perlu dikerjakan</div>
+}
+
+// Menambahkan Task
+const TaskAdder = ({ setTasks, tasks }) => {
+  const [currentValue, setCurrentValue] = useState('');
+
+  const handleAddTask = () => {
+    const newTask = {
+      id: tasks.length + 1,
+      message: currentValue,
+    }
+    setTasks([...tasks, newTask])
+    setCurrentValue('');
+  }
+
+
+  return <div className="task-adder">
+    <TextField
+      value={currentValue}
+      onChange={event => setCurrentValue(event.target.value)}
+      label="Tambah Task"
+      variant="outlined"
+    />
+
+    <Button
+      variant="cointained"
+      color="primary"
+      disabled={currentValue === ''}
+      onClick={() => handleAddTask()}>
+      Tambah
+    </Button>
+
+  </div>
+}
+
+
+// Memunculkan Task apa saja
+const Task = ({ message, id, setTasks, tasks }) => {
+
+  const handleDelete = () => {
+    const updatedTasks = tasks.filter(task => task.id !== id);
+    setTasks(updatedTasks);
+  }
+
+  return <div className="container-task">
+    <div className="task">
+      <div style={{ display: "inline-block" }}>
+        {message}
+      </div>
+
+      <Button
+        variant="contained"
+        color="secondary"
+        startIcon={<DeleteIcon></DeleteIcon>}
+        onClick={() => handleDelete()}
+      >
+        Delete
+      </Button>
     </div>
-  );
+  </div>
+}
+
+
+
+// Melakukan perulangan dari total Task pada array Data
+const TaskList = ({ tasks, setTasks }) => {
+  return tasks.map((task, index) => {
+    return <Task message={task.message} id={task.id} setTasks={setTasks} tasks={tasks} key={index} />
+  })
+}
+
+// MAIN
+const TaskApp = () => {
+  const [tasks, setTasks] = useState([]);
+
+  return <div className="container">
+    <div id="child">
+      <Header />
+      <InfoBar taskNumber={tasks.length} />
+      <TaskAdder setTasks={setTasks} tasks={tasks} />
+      <TaskList tasks={tasks} setTasks={setTasks} />
+    </div>
+  </div>
+}
+
+
+
+
+const App = () => {
+  return <>
+    <TaskApp></TaskApp>
+  </>
 }
 
 export default App;
